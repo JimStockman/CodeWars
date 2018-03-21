9bed3e3b95da46186aecc6296b95018af6eef51d
STRYK3R23 <jimstockman44@gmail.com>
2018-03-21 16:23:16 +0100

    ptrerr

--- PrinterErrors/prtErr.js
+++ PrinterErrors/prtErr.js
@@ -1,3 +1,27 @@
+// Passed !
+
 function printerError(s) {
-    
+  let printer = ("abcdefghijklm");
+  
+  let count = s.length;
+  for (let x = 0; x < s.length; x++) {
+    for (let y = 0; y < printer.length; y++) {
+      if (s[x] == printer[y]) {
+        count--;
+      }
+    }
+  }
+  return count  + "/" + s.length;
 }
+
+console.log(printerError("aaabbbbhaijjjmzzzz"));
+
+
+/*
+
+//Best solution
+function printerError(s) {
+    return s.match(/[^a-m]/g).length + "/" + s.length;
+}
+
+*/
