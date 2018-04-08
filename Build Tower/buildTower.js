74949e82f95eb0115a37a17b97df46dcf62e673b
STRYK3R23 <jimstockman44@gmail.com>
2018-04-08 14:46:50 +0200

    Update

--- Build Tower/buildTower.js
+++ Build Tower/buildTower.js
@@ -1,15 +1,14 @@
 function towerBuilder(nFloors) {
   let building = [];
-  let flrs = 1
-  for (let x = 0; x < nFloors; x++) {
-    for (let y = 0; y < flrs; y++) {
-      building[x] += '*'
-    }
-   flrs += 2
+  let dots = 1;
+  let maxDots = nFloors*2-2;
+
+  for (let i = 0; i < nFloors; i++) {
+    building[i] = ' '.repeat(maxDots/2 - i) + '*'.repeat(dots) + ' '.repeat(maxDots/2 - i)
+    dots += 2
   }
+
   return building;
 }
 
-towerBuilder(3).forEach(element => {
-  console.log(element)
-});
+console.log(towerBuilder(3))
